7ed8a79d6a76380142ce2be98c59bfcb2c380fc1
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-07-21 13:26:42 -0600

    poketable looking excellent

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,10 +2,19 @@ var express = require('express');
 var router = express.Router();
 var queries = require('../lib/queries')
 
-router.get('/', (req, res, next) => {
+router.route('/') 
+  .get((req, res, next) => {
   queries.getAllPokemon().then((resp) => {
      res.render('pokemon/index', {pokemon:resp});
   })
 })
+  .post((req, res, next) => {
+
+  })
+
+router.route('/new')
+  .get((req, res, next) => {
+
+  })
 
 module.exports = router;
