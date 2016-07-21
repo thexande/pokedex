c6c3017ac16193b21d8e26f27ff93230def2d8d7
marthaberner <marthaberner.mb@gmail.com>
2016-07-20 21:22:34 -0600

    add trainer_id values to pokemon seeds

--- seeds/add_pokemon.js
+++ seeds/add_pokemon.js
@@ -1,20 +1,30 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
-    // Deletes ALL existing entries
     knex('pokemon').del(),
 
-    // Inserts seed entries
-    knex('pokemon').insert({id: 1, name: "Bulbasaur", CP: 15, in_gym: false }),
-    knex('pokemon').insert({id: 2, name: "Ivysaur", CP: 60, in_gym: false}),
-    knex('pokemon').insert({id: 3, name: "Venasaur", CP: 100, in_gym: false}),
-    knex('pokemon').insert({id: 4, name: "Charmander", CP: 15, in_gym: false}),
-    knex('pokemon').insert({id: 5, name: "Charmeleon", CP: 55, in_gym: false}),
-    knex('pokemon').insert({id: 6, name: "Charizard", CP: 99, in_gym: false}),
-    knex('pokemon').insert({id: 7, name: "Squirtle", CP: 19, in_gym: false}),
-    knex('pokemon').insert({id: 8, name: "Wartortle", CP: 22, in_gym: false}),
-    knex('pokemon').insert({id: 9, name: "Blastoise", CP: 111, in_gym: false}),
-    knex('pokemon').insert({id: 10, name: "Caterpie", CP: 5, in_gym: false}),
-    knex('pokemon').insert({id: 11, name: "Metapod", CP: 18, in_gym: false}),
-    knex('pokemon').insert({id: 12, name: "Butterfree", CP: 104, in_gym: false})
+    knex('pokemon').insert({id: 1, name: "Bulbasaur",
+      cp: 15, in_gym: false, trainer_id: 1 }),
+    knex('pokemon').insert({id: 2, name: "Ivysaur",
+      cp: 60, in_gym: false, trainer_id: 1}),
+    knex('pokemon').insert({id: 3, name: "Venasaur",
+      cp: 100, in_gym: false, trainer_id: 2}),
+    knex('pokemon').insert({id: 4, name: "Charmander",
+      cp: 15, in_gym: false, trainer_id: 2}),
+    knex('pokemon').insert({id: 5, name: "Charmeleon",
+      cp: 55, in_gym: false, trainer_id: 2}),
+    knex('pokemon').insert({id: 6, name: "Charizard",
+      cp: 99, in_gym: false, trainer_id: 3}),
+    knex('pokemon').insert({id: 7, name: "Squirtle",
+      cp: 19, in_gym: false, trainer_id: 4}),
+    knex('pokemon').insert({id: 8, name: "Wartortle",
+      cp: 22, in_gym: false, trainer_id: 4}),
+    knex('pokemon').insert({id: 9, name: "Blastoise",
+      cp: 111, in_gym: false, trainer_id: 4}),
+    knex('pokemon').insert({id: 10, name: "Caterpie",
+      cp: 5, in_gym: false, trainer_id: 3}),
+    knex('pokemon').insert({id: 11, name: "Metapod",
+      cp: 18, in_gym: false, trainer_id: 1}),
+    knex('pokemon').insert({id: 12, name: "Butterfree",
+     cp: 104, in_gym: false, trainer_id: 1})
   );
 };
