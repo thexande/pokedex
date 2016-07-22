7695b74e0cd21adfca24324c6f27b4617a617380
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-07-22 16:28:57 -0600

    working on gym page

--- routes/gym.js
+++ routes/gym.js
@@ -7,11 +7,25 @@ router.get('/', (req, res, next) => {
 })
 
 router.get('/:id/add', (req, res, next) => {
-    queries.updatePokemon({
-    id: req.params.id,
-    in_gym: true
-  }).then((resp) =>{
-    res.redirect('/pokemon')
-  })
+    queries.getPokemonById(req.params.id).then((resp) => {
+      queries.updatePokemon({
+        id: resp[0].id,
+        in_gym: !resp[0].in_gym
+      }).then((resp) => {
+         // check if there are 2 pokemon in the gym
+        queries.getAllPokemon().then((resp) => {
+          var pokemonInGym = resp.filter((val) => {
+            return val.in_gym == true
+          })
+          console.log(pokemonInGym)
+          if(pokemonInGym.length >= 2){
+            res.render('gym', {pokemonInGym: pokemonInGym})
+          }else{
+          res.redirect('/pokemon')
+          }
+        })
+      })
+    })
+
 })
 module.exports = router
