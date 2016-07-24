c621cb6234c8c39cbaf0d39d0f25574467905b48
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-07-24 14:56:43 -0600

    gym battle drop downs working

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,6 +3,64 @@ $(document).ready(function() {
     $('.mselect').material_select();
     $('#trainerSelectEdit').val($("#trainerId").val());
     $('#trainerSelectEdit').material_select();
+
+    $('#gymPoke1Select').val($('#pokemon_1_id').val());
+    $('#gymPoke1Select').material_select()
+
+    $('#gymPoke2Select').val($('#pokemon_2_id').val());
+    $('#gymPoke2Select').material_select();
+    
+    // handle gym 1 pokemon change
+    $('#gymPoke1Select').change((event) => {
+        var newPoke1Id = $('#gymPoke1Select').val()
+        var oldPoke1Id = $('[name = "pokemon_1_id"]').val()
+        $.ajax({
+            method: "POST",
+            url: '/gym/updateGymPokemon',
+            data: {
+                newPokeId: newPoke1Id,
+                oldPokeId: oldPoke1Id,
+                gymPosition: 1
+            }
+        }).then((resp) => {
+            // set h3 and select box for new pokemon
+            $('#gymPoke1Select').val(resp[0].id);
+            $('#gymPoke1Select').material_select()
+            $('#poke1Title').html(resp[0].name)
+            $('#poke1Cp').html(resp[0].cp)
+            $('#pokemon_1_cp').val(resp[0].cp)
+            $('#pokemon_1_id').val(resp[0].id)
+            $('#pokemon_1_name').val(resp[0].name)
+            console.log(resp)
+        })
+    })
+    // handle gym 2 pokemon change
+    $('#gymPoke2Select').change((event) => {
+        var newPoke2Id = $('#gymPoke2Select').val()
+        var oldPoke2Id = $('[name = "pokemon_2_id"]').val()
+        $.ajax({
+            method: "POST",
+            url: '/gym/updateGymPokemon',
+            data: {
+                newPokeId: newPoke2Id,
+                oldPokeId: oldPoke2Id,
+                gymPosition: 2
+            }
+        }).then((resp) => {
+            // set h3 and select box for new pokemon
+            $('#gymPoke2Select').val(resp[0].id);
+            $('#gymPoke2Select').material_select()
+            $('#poke2Title').html(resp[0].name)
+            $('#poke2Cp').html(resp[0].cp)
+             $('#pokemon_2_cp').val(resp[0].cp)
+            $('#pokemon_2_id').val(resp[0].id)
+            $('#pokemon_2_name').val(resp[0].name)
+            console.log(resp)
+        })
+    })
+
+
+
     $(".button-collapse").sideNav();
     $("#trainerSelect").val()
     $('#pokedex').DataTable( {
--- routes/gym.js
+++ routes/gym.js
@@ -3,9 +3,46 @@ var router = express.Router();
 var queries = require('../lib/queries')
 
 router.get('/', (req, res, next) => {
-  res.render('gym')
+  queries.getAllPokemon().then((allPokemon) => {
+    var pokemonInGym = allPokemon.filter((val) => {
+          return val.in_gym == true        
+    })
+    console.log(pokemonInGym);
+    
+    res.render('gym', {
+      pokemon: allPokemon,
+      pokemonInGym: pokemonInGym
+    })
+  })
 })
+
+router.post('/updateGymPokemon', (req, res, next) => {
+  // set old to false
+// res.json(req.body)
+  queries.updatePokemon({
+    id: parseInt(req.body.oldPokeId),
+    in_gym: false
+  }).then((resp) => {
+    // update new pokemon 1 to in gym
+    console.log('old poke set to false');
+    
+    queries.updatePokemon({
+      id: parseInt(req.body.newPokeId),
+      in_gym: true
+    }).then((resp) => {
+      console.log('new poke set to true');
+      // pokemon updated. send update pokemon 1 information
+      queries.getPokemonById(req.body.newPokeId).then((resp) => {
+        res.json(resp)
+      })
+    })
+  })
+  // set new to true
+})
+
 router.post('/', (req, res, next) => {
+  res.json(req.body)
+
   if(parseInt(req.body.pokemon_1_cp) > parseInt(req.body.pokemon_2_cp)){
     // player 1 win
     var winner = req.body.pokemon_1_name
@@ -32,7 +69,7 @@ router.get('/:id/add', (req, res, next) => {
           })
           console.log(pokemonInGym)
           if(pokemonInGym.length >= 2){
-            res.render('gym', {pokemonInGym: pokemonInGym})
+            res.redirect('/gym')
           }else{
           res.redirect('/pokemon')
           }
